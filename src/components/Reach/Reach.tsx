729b49bd432f221296fe031db51352fcd6e94110
dij0s <redrouge03@gmail.com>
2025-01-13 22:10:23 +0100

    Added robot doodle and updated favicon..

--- src/components/Reach/Reach.tsx
+++ src/components/Reach/Reach.tsx
@@ -2,18 +2,21 @@ import "./Reach.css";
 
 function Reach() {
   return (
-    <div id="reach-wrapper">
-      <div id="reach-doodle-line" />
-      <h1>Let's solve together!</h1>
-      <div
-        id="reach-action"
-        onClick={() =>
-          (window.location.href = "mailto:dion.08osmani@gmail.com")
-        }
-      >
-        <h2>Get in touch</h2>
+    <>
+      <div id="reach-wrapper">
+        <div id="reach-doodle-line" />
+        <h1>Let's solve together!</h1>
+        <div
+          id="reach-action"
+          onClick={() =>
+            (window.location.href = "mailto:dion.08osmani@gmail.com")
+          }
+        >
+          <h2>Get in touch</h2>
+        </div>
       </div>
-    </div>
+      <div id="reach-doodle-robot" />
+    </>
   );
 }
 
